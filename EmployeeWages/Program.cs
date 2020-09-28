f1889a6bd412563e8fa1a916a24c1278479a3dcd
sumitpandit <sumitpandit178@gmail.com>
2020-09-28 17:02:55 +0530

    [Sumit] Refactor: Calculating Wages for a Month

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -9,25 +9,32 @@ namespace EmployeeWages
             int Full_Time = 2;
             int Part_Time = 1;
             int Emp_rate = 20;
-            int Emp_Hour, Emp_Wages;
+            int No_Of_Working_Days = 20;
+            int Emp_Hour, Emp_Wages = 0;
             Random r = new Random();
-            int check = r.Next(1, 4);
-
-            switch (check)
+            for (int i = 0; i < No_Of_Working_Days; i++)
             {
-                case 2:
-                    Emp_Hour = 8;
-                    break;
-                case 1:
-                    Emp_Hour = 4;
-                    break;
-                default:
-                    Emp_Hour = 0;
-                    break;
-            }
 
-            Emp_Wages = Emp_Hour * Emp_rate;
+                int check = r.Next(1, 4);
+
+                switch (check)
+                {
+                    case 2:
+                        Emp_Hour = 8;
+                        break;
+                    case 1:
+                        Emp_Hour = 4;
+                        break;
+                    default:
+                        Emp_Hour = 0;
+                        break;
+                }
+
+                Emp_Wages += Emp_Hour * Emp_rate;
+
+            }
             Console.WriteLine("Employee Wages: " + Emp_Wages);
+
         }
     }
 }
