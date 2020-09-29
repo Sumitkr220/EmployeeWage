ebc478d396ee0990357e0a25c989ae6376db61d1
sumitpandit <sumitpandit178@gmail.com>
2020-09-29 14:35:36 +0530

    [Sumit] Refactor: Computing Employee Wage and Save by Company

---
+++ EmployeeWages/EmpWageBuilderObject.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class EmpWageBuilderObject
+    {
+        public const int FULL_TIME = 2;
+        public const int PART_TIME = 1;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            Random random = new Random();
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                int check = random.Next(1, 4);
+                switch (check)
+                {
+                    case 2:
+                        empHrs = 8;
+                        break;
+                    case 1:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days#:" + totalWorkingDays + " Emp Hrs :" + empHrs);
+            }
+            totalEmpWage = totalEmpHrs *this.empRatePerHour;
+            Console.WriteLine("Total Employee Wage for company : " + company + " is " + totalEmpWage);     
+        }
+
+        public string toString()
+        {
+            return "Total Emp Wage for company : " + this.company + " is: " + this.totalEmpWage;
+        }
+    }
+}
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -4,42 +4,14 @@ namespace EmployeeWages
 {
     class Program
     {
-        public const int FULL_TIME = 2;
-        public const int PART_TIME = 1;
-        public static int ComputeEmpWage(string company,int empRate,int totalOfWorkingDays,int maxHoursPerMonth)
-        {
-            int noOfWorkingDays = 0;
-            int empHour=0;
-            int totalEmpWages = 0;
-            int workingHour = 0;
-            Random r = new Random();
-            while (noOfWorkingDays < totalOfWorkingDays && workingHour < maxHoursPerMonth)
-            {
-                noOfWorkingDays++;
-                int check = r.Next(1, 4);
-                switch (check)
-                {
-                    case 2:
-                        empHour = 8;
-                        break;
-                    case 1:
-                        empHour = 4;
-                        break;
-                    default:
-                        empHour = 0;
-                        break;
-                }
-                workingHour += empHour;
-                Console.WriteLine("Days#:" + noOfWorkingDays + " Emp Hrs :" + empHour);
-            }
-            totalEmpWages = workingHour * empRate;
-            Console.WriteLine("Total Employee Wage for company : "+company+" is " + totalEmpWages);
-            return totalEmpWages;
-        }
         static void Main(string[] args)
         {
-            ComputeEmpWage("DMart",20,20,100);
-            ComputeEmpWage("Reliance", 10, 18, 200);
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 10, 200);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 15, 20, 100);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
         }
     }
 }
