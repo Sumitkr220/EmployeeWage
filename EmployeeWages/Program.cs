06e4db6c3097e639efcc5d42db55ed88fe8caa6a
sumitpandit <sumitpandit178@gmail.com>
2020-09-29 12:30:33 +0530

    [sumit] Refactor: Using proper notation

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -4,40 +4,38 @@ namespace EmployeeWages
 {
     class Program
     {
+        public const int FULL_TIME = 2;
+        public const int PART_TIME = 1;
+        public const int EMP_RATE = 20;
+        public const int MAX_WORKING_HOUR = 100;
+        public const int MAX_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            int Full_Time = 2;
-            int Part_Time = 1;
-            int Emp_rate = 20;
-            int No_Of_Working_Days = 0;
-            int Total_Working_Hour = 100;
-            int Emp_Hour = 0, Emp_Wages = 0;
-            int working_Hour = 0;
+            int noOfWorkingDays = 0;
+            int empHour = 0;
+            int empWages = 0;
+            int workingHour = 0;
             Random r = new Random();
-            while (No_Of_Working_Days < 20 && working_Hour < 100)
+            while (noOfWorkingDays < MAX_WORKING_DAYS && workingHour < MAX_WORKING_HOUR)
             {
-                No_Of_Working_Days++;
+                noOfWorkingDays++;
                 int check = r.Next(1, 4);
-
                 switch (check)
                 {
                     case 2:
-                        Emp_Hour = 8;
+                        empHour = 8;
                         break;
                     case 1:
-                        Emp_Hour = 4;
+                        empHour = 4;
                         break;
                     default:
-                        Emp_Hour = 0;
+                        empHour = 0;
                         break;
                 }
-                working_Hour += Emp_Hour;
-
-                Emp_Wages += Emp_Hour * Emp_rate;
-
+                workingHour += empHour;
+                empWages += empHour * EMP_RATE;
             }
-            Console.WriteLine("Employee Wages: " + Emp_Wages);
-           
+            Console.WriteLine("Employee Wages: " + empWages);  
         }
     }
 }
