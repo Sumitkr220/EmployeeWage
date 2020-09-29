abc5bdcb849489c6ef3a3b8434feca1779a176b5
sumitpandit <sumitpandit178@gmail.com>
2020-09-29 16:34:16 +0530

    [Sumit] Refactor: Blank Code Commit

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -1,17 +1,1 @@
-﻿using System;
-
-namespace EmployeeWages
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 10, 200);
-            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 15, 20, 100);
-            dMart.computeEmpWage();
-            Console.WriteLine(dMart.toString());
-            reliance.computeEmpWage();
-            Console.WriteLine(reliance.toString());
-        }
-    }
-}
+﻿
